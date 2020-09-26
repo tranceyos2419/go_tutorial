a9008c2060b934a92ebeed6a5fcd24c2d5898339
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-09-26 20:47:42 +0900

    build: learning map

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	println("nubmer")
+	for i := 0; i < 5; i++ {
+		println(i)
+	}
+
+	println("array")
+	a := []string{"Foo", "Bar"}
+	for i, s := range a {
+		fmt.Println(i, s)
+	}
+
+	println("map")
+	m := make(map[string]string)
+	m["me"] = "yoshi"
+	m["sister"] = "yuki"
+	for k, v := range m {
+		println(k, v)
+	}
 }
