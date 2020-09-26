a1868cca1f0d7f97b9f49271b1140044df5fb5ec
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-09-27 07:51:38 +0900

    build: struct

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+type person struct {
+	name string
+	age  int
+}
+
+func main() {
+	yoshi := person{name: "yoshi", age: 22}
+	fmt.Println("Hi, my name is ", yoshi.name)
+	fmt.Println(yoshi)
 }
