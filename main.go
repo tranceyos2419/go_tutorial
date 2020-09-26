8bbe3740aa8d196a19b29eff7a32a93ccfcfa289
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-09-27 07:38:02 +0900

    build: error

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
 func main() {
-	r := sum(2, 3)
-	println(r)
+	res, err := sqrt(16)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res)
+	}
 
 }
 
-func sum(x int, y int) int {
-	return x + y
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, errors.New("Undefined for negative numbers")
+	}
+	return math.Sqrt(x), nil
 }
