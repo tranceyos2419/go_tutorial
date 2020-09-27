aea9241fd96016a2a98912b6032899d139344a7b
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-09-27 10:21:55 +0900

    build: get a http reqeust

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
+func main() {
+	url := "https://jsonplaceholder.typicode.com/todos/1"
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+
+		panic(err)
+	}
+
+	fmt.Println(string(body))
 }
