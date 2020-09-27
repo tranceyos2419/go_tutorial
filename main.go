f8819ce5854f75e47d0509eb65a2ed37ab1a30fe
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-09-27 14:04:32 +0900

    build: muliple return

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	//sum
 	r := sum(2, 3)
 	println(r)
+
+	// div
 	fmt.Println(div(6, 3))
 
+	// swap
+	a, b := swap("cat", "dog")
+	fmt.Println(a, b)
 }
 
 func sum(x int, y int) int {
@@ -17,4 +23,8 @@ func sum(x int, y int) int {
 func div(x, y int) (z int) {
 	z = x / y
 	return
+}
+
+func swap(s1, s2 string) (string, string) {
+	return s2, s1
 }
