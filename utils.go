628646458861019d52b52eb0d7cb2ce064bccaf6
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-09-27 10:00:54 +0900

    build: packages

---
+++ utils.go
@@ -0,0 +1,12 @@
+// Because our ReverseRunes function begins with an upper-case letter, it is exported,
+package main
+
+import "fmt"
+
+func Demo() {
+	fmt.Println("HI")
+}
+
+func Sum(x int, y int) int {
+	return x + y
+}
