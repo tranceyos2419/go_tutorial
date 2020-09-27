32893765f843c30a37e3e46981a9caf336eddb54
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-09-28 07:17:46 +0900

    more than 2 structs

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import "fmt"
 
 type person struct {
-	name string
-	age  int
+	name     string
+	age      int
+	passport Passport
+}
+
+type Passport struct {
+	nationatlity string
 }
 
 func main() {
-	yoshi := person{name: "yoshi", age: 22}
-	fmt.Println("Hi, my name is ", yoshi.name)
+	yoshi := person{name: "yoshi", age: 22, passport: Passport{nationatlity: "japan"}}
+	fmt.Println("Hi, my name is ", yoshi.name, "I am from", yoshi.passport.nationatlity)
 	fmt.Println(yoshi)
 }
