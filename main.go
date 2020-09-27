9bc238a914f0185d61e42d53303c296c245b0cf8
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-09-28 07:10:04 +0900

    build: delete

--- main.go
+++ main.go
@@ -6,7 +6,11 @@ import (
 
 func main() {
 	slice := []int{1, 2, 3}
-	slice = append(slice, 1)
-	// a := [2][2]int{}
+	slice = append(slice[:0], slice[1:]...) // delete
+	slice = append(slice, 1)                // add
 	fmt.Println(slice)
+	//
+	cat := []string{"sleepy"}
+	cat = append(cat, "kiss")
+	fmt.Println(cat)
 }
