4bc40e149ef7c8943837d2acbf2b19f29ff48b71
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-09-27 15:23:29 +0900

    buld: defer, panic, recover

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+)
 
+func main() {
+	fmt.Println("start")
+	panicker()
+	fmt.Println("end")
+}
+func panicker() {
+	fmt.Println("about to panic")
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("Error: ", err)
+		}
+	}()
+	panic("something bad happen")
+	fmt.Println("the end of panic")
 }
