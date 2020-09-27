23faa507115275c02a4d90010e427f905dcd9e1a
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-09-27 14:02:18 +0900

    build: named return

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 	r := sum(2, 3)
 	println(r)
+	fmt.Println(div(6, 3))
 
 }
 
 func sum(x int, y int) int {
 	return x + y
+}
+
+// named return
+func div(x, y int) (z int) {
+	z = x / y
+	return
 }
