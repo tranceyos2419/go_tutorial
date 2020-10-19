98bfa14de6b86bb74060a1203ac15650108e04fd
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-10-19 09:11:06 +0900

    build: sucessuflly parsing tones

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -15,7 +16,55 @@ type Ttodo struct {
 	Completed bool   `json:"completed"`
 }
 
+type DocumentTone struct {
+	Tones []Tone `json:"tones"`
+}
+
+type Tone struct {
+	Score    float64 `json:"score"`
+	ToneId   string  `json:"tone_id"`
+	ToneName string  `json:"tone_name"`
+}
+
 func main() {
+	testPost()
+	// testGet()
+
+}
+
+func testPost() {
+	url := "http://localhost:8080/analyze"
+
+	text := "Fuck off. I don't know who you are.sales have been disappointing for the past three quarters. We have a competitive product, but we need to do a better job of selling it! "
+	values := map[string]string{"text": text}
+
+	jsonValue, _ := json.Marshal(values)
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonValue))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+
+	// var documentTone DocumentTone
+	var tones []Tone
+	json.Unmarshal(body, &tones)
+	for i, tone := range tones {
+		fmt.Println(i, tone)
+	}
+}
+
+func testGet() {
 	url := "https://jsonplaceholder.typicode.com/todos"
 
 	resp, err := http.Get(url)
