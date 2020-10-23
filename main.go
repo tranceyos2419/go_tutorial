771c6a0f9659c7cb0bd65825a4b8f67973ccd6d9
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-10-23 17:43:47 +0900

    build: built interface

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
+type Shape interface {
+	Area()
+	Volume()
+}
+
+type Cube struct {
+	Height float64
+}
+
+func (c Cube) Area() float64 {
+	return 6 * (c.Height * c.Height)
+}
+
+func (c Cube) Volume() float64 {
+	return c.Height * c.Height * c.Height
+}
 func main() {
 
+	c := Cube{Height: 165}
+	fmt.Println("Area: ", c.Area())
+	fmt.Println("Volume: ", c.Volume())
 }
